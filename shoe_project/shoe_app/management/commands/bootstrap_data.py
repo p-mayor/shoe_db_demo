61bf81fb8b56d85138b35d47dbe6acc735afc157
petem14 <petem14@gmail.com>
2019-11-26 13:00:21 -0500

    format

--- shoe_project/shoe_app/management/commands/bootstrap_data.py
+++ shoe_project/shoe_app/management/commands/bootstrap_data.py
@@ -10,24 +10,6 @@ from shoe_project.shoe_app.models import ShoeType, ShoeColor
 # dress
 # other
 
-
-class Command(BaseCommand):
-    help = 'Populate the ShoeType and ShoeColor tables'
-
-    def handle(self, *args, **options):
-        init_shoe_types = ['sneaker', 'boot', 'sandal', 'dress', 'other']
-        init_shoe_colors = ['Red','Orange','Yellow','Green','Blue','Indigo', 'Violet', 'White', 'Black']
-        for i in init_shoe_types:
-            new_shoe_type = ShoeType(style=i)
-            new_shoe_type.save()
-            self.stdout.write(self.style.SUCCESS('Successfully created ShoeType "%s"' % i))
-        
-        for i in init_shoe_colors:
-            new_shoe_color = ShoeColor(color_name=i)
-            new_shoe_color.save()
-            self.stdout.write(self.style.SUCCESS('Successfully created ShoeColor "%s"' % i))
-
-
 #  Populate the ShoeColor table with the following entries:
 # Red
 # Orange
@@ -38,3 +20,21 @@ class Command(BaseCommand):
 # Violet
 # White
 # Black
+
+
+class Command(BaseCommand):
+    help = 'Populate the ShoeType and ShoeColor tables'
+
+    def handle(self, *args, **options):
+        init_shoe_types = ['sneaker', 'boot', 'sandal', 'dress', 'other']
+        init_shoe_colors = ['Red', 'Orange', 'Yellow', 'Green',
+                            'Blue', 'Indigo', 'Violet', 'White', 'Black']
+        for i in init_shoe_types:
+            new_shoe_type = ShoeType(style=i)
+            new_shoe_type.save()
+            self.stdout.write(self.style.SUCCESS('Successfully created ShoeType "%s"' % i))
+
+        for i in init_shoe_colors:
+            new_shoe_color = ShoeColor(color_name=i)
+            new_shoe_color.save()
+            self.stdout.write(self.style.SUCCESS('Successfully created ShoeColor "%s"' % i))
